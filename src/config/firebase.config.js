01b87b6c8efbb4b5426ec7904db8a2b357bdaed5
ducanh11092001@gmail.com <ducanh11092001@gmail.com>
2022-11-06 08:39:52 +0700

    config firebase

---
+++ src/config/firebase.config.js
@@ -0,0 +1,18 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+// getApps: number of apps, if > 0 get else init a new one (should only init once)
+const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+export { app, storage };
