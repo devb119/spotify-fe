6d74ab419a4dcb952c723fb6a25fd1da9f1401df
ducanh11092001@gmail.com <ducanh11092001@gmail.com>
2022-11-07 15:46:31 +0700

    add active style

---
+++ src/utils/styles.js
@@ -0,0 +1,5 @@
+export const isActiveStyles =
+  "text-lg mx-5 text-headingColor font-semibold hover:text-headingColor duration-100 transition-all ease-in-out";
+
+export const isNotActiveStyles =
+  "text-lg mx-5 text-textColor font-semibold hover:text-headingColor duration-100 transition-all ease-in-out";
