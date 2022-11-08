6e7c446ab439d3bcef44a85d6bc610540bc10d54
ducanh11092001@gmail.com <ducanh11092001@gmail.com>
2022-11-08 11:47:40 +0700

    add user context

--- src/App.js
+++ src/App.js
@@ -4,33 +4,42 @@ import { Home, Login } from "./components";
 import { app } from "./config/firebase.config";
 import { getAuth } from "firebase/auth";
 import { AnimatePresence } from "framer-motion";
+import { validateUser } from "./api";
+import { useStateValue } from "./context/StateProvider";
+import { actionType } from "./context/reducer";
 
 const App = () => {
+  // eslint-disable-next-line no-unused-vars
   const [auth, setAuth] = useState(
     false || window.localStorage.getItem("auth") === "true"
   );
 
   const fireBaseAuth = getAuth(app);
   const navigate = useNavigate();
+  // eslint-disable-next-line no-unused-vars
+  const [{ user }, dispatch] = useStateValue();
 
   useEffect(() => {
     // Observe log in state
     fireBaseAuth.onAuthStateChanged((userCred) => {
       if (userCred) {
         userCred.getIdToken().then((token) => {
-          console.log(token);
+          validateUser(token).then((data) => {
+            dispatch({ type: actionType.SET_USER, user: data });
+          });
         });
         // If the token is expired, immediately redirect to login page
       } else {
         setAuth(false);
         window.localStorage.setItem("auth", "false");
+        dispatch({ type: actionType.SET_USER, user: null });
         navigate("/login");
       }
     });
-  }, [navigate, fireBaseAuth]);
+  }, [navigate, fireBaseAuth, dispatch]);
 
   return (
-    <AnimatePresence exitBeforeEnter>
+    <AnimatePresence mode="wait">
       <div className="h-auto min-w-{680px} bg-primary flex justify-center items-center">
         <Routes>
           <Route path="/login" element={<Login setAuth={setAuth} />} />
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { StateProvider } from "./context/StateProvider";
+import { initialState } from "./context/initialState";
+import reducer from "./context/reducer";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <App />
+      </StateProvider>
     </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
