4461686f3b70652580f2adeccdb39a1d259f125c
ducanh11092001@gmail.com <ducanh11092001@gmail.com>
2022-11-16 12:26:03 +0700

    init card

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MusicCard() {
+  return (
+    <div className="p-4 w-40 gap-3 h-auto rounded-lg shadow-md bg-blue-400">
+      <p>User card</p>
+    </div>
+  );
+}
+
+export default MusicCard;
--- src/components/index.js
+++ src/components/index.js
@@ -7,3 +7,8 @@ export { default as Dashboard } from "./Dashboard";
 export { default as DashboardHome } from "./DashboardHome";
 export { default as DashboardSongs } from "./DashboardSongs";
 export { default as DashboardUsers } from "./DashboardUsers";
+export { default as DashboardArtists } from "./DashboardArtists";
+export { default as DashboardAlbums } from "./DashboardAlbums";
+
+// CARD COMPONENTS
+export { default as MusicCard } from "./MusicCard";
