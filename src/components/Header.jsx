8d632995a73d44b6dcd8884b9e8ada53cef106b8
ashley <ashley.nguyen@avepoint.com>
2022-11-19 00:00:17 +0700

    edit header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,53 +29,6 @@ function Header() {
   };
   return (
     <header className="flex items-center w-full p-4 md:py-2 md:px-6">
-      <NavLink to="/">
-        <img src={Logo} alt="Logo" className="w-40" />
-      </NavLink>
-
-      <ul className="flex items-center justify-center ml-7">
-        <li>
-          <NavLink
-            to="/home"
-            className={({ isActive }) =>
-              isActive ? isActiveStyles : isNotActiveStyles
-            }
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? isActiveStyles : isNotActiveStyles
-            }
-            to="/music"
-          >
-            Music
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? isActiveStyles : isNotActiveStyles
-            }
-            to="/premium"
-          >
-            Premium
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? isActiveStyles : isNotActiveStyles
-            }
-            to="/contact"
-          >
-            Contact Us
-          </NavLink>
-        </li>
-      </ul>
-
       <div
         className="flex items-center ml-auto cursor-pointer gap-2 relative"
         onMouseEnter={() => setIsMenuOpen(true)}
