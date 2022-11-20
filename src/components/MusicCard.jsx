f5b9f8c39a7e86c0ce0848a609661e2a29f3716a
ducanh11092001@gmail.com <ducanh11092001@gmail.com>
2022-11-20 14:54:12 +0700

    fix card size

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -8,20 +8,20 @@ function MusicCard({ name, imageURL, artists }) {
   const hideIcon = () => setShowPlay(false);
   return (
     <div
-      className="p-4 w-52 h-auto rounded-lg shadow-md bg-cardBg hover:bg-cardBgLight transition-all duration-200 cursor-pointer relative"
+      className="p-4 w-48 h-auto rounded-lg shadow-md bg-cardBg hover:bg-cardBgLight transition-all duration-200 cursor-pointer relative"
       onMouseEnter={showIcon}
       onMouseLeave={hideIcon}
     >
-      <img src={imageURL} alt="song cover" className="rounded-lg w-full mb-4" />
+      <img src={imageURL} alt="song cover" className="rounded-lg w-full mb-3" />
       <div
         className={`h-12 w-12 bg-green-500 flex justify-center items-center rounded-full absolute right-7 top-1/2 ${
-          showPlay ? "opacity-100 -translate-y-2" : "opacity-0"
+          showPlay ? "opacity-100 -translate-y-3" : "opacity-0"
         } transition-all duration-200`}
       >
         <GrPlayFill className="text-xl" />
       </div>
-      <p className="text-md text-white font-semibold mb-4">
-        {name.length > 20 ? `${name.slice(0, 20)}...` : name}
+      <p className="text-md text-white font-semibold mb-2">
+        {name.length > 17 ? `${name.slice(0, 17)}...` : name}
       </p>
       <p className="text-sm text-textColor font-semibold">{artists}</p>
     </div>
