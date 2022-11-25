4b8834d814180bb6df751ecb34b201efa667168a
ducanh11092001@gmail.com <ducanh11092001@gmail.com>
2022-11-25 17:21:01 +0700

    edit music card

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
 import { GrPlayFill } from "react-icons/gr";
+import { actionType } from "../context/reducer";
+import { useStateValue } from "../context/StateProvider";
 
-function MusicCard({ name, imageURL, artists }) {
+function MusicCard({ song }) {
   const [showPlay, setShowPlay] = useState(false);
+  const [{ isSongPlaying, currentSong }, dispatch] = useStateValue();
 
   const showIcon = () => setShowPlay(true);
   const hideIcon = () => setShowPlay(false);
-  const addToContext = () => {};
+  const addToContext = () => {
+    if (!isSongPlaying) {
+      dispatch({ type: actionType.SET_IS_SONG_PLAYING, isSongPlaying: true });
+    }
+    if (currentSong?._id !== song._id) {
+      dispatch({ type: actionType.SET_CURRENT_SONG, currentSong: song });
+    }
+  };
   return (
     <div
       className="p-4 w-48 h-auto rounded-lg shadow-md bg-cardBg hover:bg-cardBgLight transition-all duration-200 cursor-pointer relative"
@@ -14,7 +24,11 @@ function MusicCard({ name, imageURL, artists }) {
       onMouseLeave={hideIcon}
       onClick={addToContext}
     >
-      <img src={imageURL} alt="song cover" className="rounded-lg w-full mb-3" />
+      <img
+        src={song.imageURL}
+        alt="song cover"
+        className="rounded-lg w-full mb-3"
+      />
       <div
         className={`h-12 w-12 bg-green-500 flex justify-center items-center rounded-full absolute right-7 top-1/2 ${
           showPlay ? "opacity-100 -translate-y-3" : "opacity-0"
@@ -23,9 +37,9 @@ function MusicCard({ name, imageURL, artists }) {
         <GrPlayFill className="text-xl" />
       </div>
       <p className="text-md text-white font-semibold mb-2">
-        {name.length > 17 ? `${name.slice(0, 17)}...` : name}
+        {song.name.length > 17 ? `${song.name.slice(0, 17)}...` : song.name}
       </p>
-      <p className="text-sm text-textColor font-semibold">{artists}</p>
+      <p className="text-sm text-textColor font-semibold">{song.artist}</p>
     </div>
   );
 }
