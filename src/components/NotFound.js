6cb60f9f408192ec8b1d4fbd199ee6c3d27cd084
ducanh11092001@gmail.com <ducanh11092001@gmail.com>
2022-11-28 10:02:40 +0700

    fix not found page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,19 +4,21 @@ import { Icon } from "../assets/img/icon";
 function NotFound() {
   return (
     <div className="text-white w-full h-screen flex flex-col items-center justify-center">
-      <img className="w-12 h-12" src={Icon} alt=""></img>
-      <h1 className="mt-10 text-3xl text-center font-bold content-center">
+      <img className="w-16 h-16" src={Icon} alt=""></img>
+      <h1 className="mt-10 text-4xl text-center font-bold content-center">
         Page not found
       </h1>
-      <p className="text-xs m-5 text-center font-bold">
+      <p className="text-sm m-5 text-center font-bold">
         We can't seem to find the page you are looking for.
       </p>
-      <button className=" mt-5 text-center  bg-white text-black p-8  hover:bg-gray-200 text-xs  font-bold text-white py-2 px-4 rounded-full">
-        Home
-      </button>
+      <Link to="/">
+        <button className=" mt-5 text-center  bg-white text-black p-8  hover:bg-gray-200 text-sm font-bold py-2 px-4 rounded-full">
+          Home
+        </button>
+      </Link>
 
       <div className="text-center mt-5">
-        <Link to="#" className="text-xs font-bold text-white">
+        <Link to="#" className="text-sm font-bold text-white">
           Help
         </Link>
       </div>
