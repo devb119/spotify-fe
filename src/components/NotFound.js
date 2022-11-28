0b276edee627465fd704aabd68034376219ba513
ducanh11092001@gmail.com <ducanh11092001@gmail.com>
2022-11-28 09:57:56 +0700

    fix not found

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,23 +3,19 @@ import { Link } from "react-router-dom";
 import { Icon } from "../assets/img/icon";
 function NotFound() {
   return (
-    <div className="text-white w-full h-screen items-center content-center">
-      <div className="mt-20 text-center">
-        <img className="w-12 h-12 m-auto" src={Icon} alt=""></img>
-      </div>
-      <h1 className=" mt-10 text-3xl text-center  font-bold content-center  m-auto">
+    <div className="text-white w-full h-screen flex flex-col items-center justify-center">
+      <img className="w-12 h-12" src={Icon} alt=""></img>
+      <h1 className="mt-10 text-3xl text-center font-bold content-center">
         Page not found
       </h1>
       <p className="text-xs m-5 text-center font-bold">
         We can't seem to find the page you are looking for.
       </p>
-      <div className="text-center ">
-        <button className=" mt-5 text-center  bg-white text-black p-8  hover:bg-gray-200 text-xs  font-bold text-white py-2 px-4 rounded-full">
-          Home
-        </button>
-      </div>
+      <button className=" mt-5 text-center  bg-white text-black p-8  hover:bg-gray-200 text-xs  font-bold text-white py-2 px-4 rounded-full">
+        Home
+      </button>
 
-      <div className="text-center  mt-5">
+      <div className="text-center mt-5">
         <Link to="#" className="text-xs font-bold text-white">
           Help
         </Link>
