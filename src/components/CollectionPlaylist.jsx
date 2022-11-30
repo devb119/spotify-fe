645119394524805bf95a121745cda1392fe46dbd
ashley <ashley.nguyen@avepoint.com>
2022-11-30 20:25:02 +0700

    add collection playlist component

---
+++ src/components/CollectionPlaylist.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CollectionPlaylist() {
+  return (
+    <div className="text-white p-8 pt-0">
+      <p className="mb-4 font-bold text-white text-xl">Playlists</p>
+
+      <div></div>
+    </div>
+  );
+}
+
+export default CollectionPlaylist;
