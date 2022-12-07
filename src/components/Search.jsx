65994107edb958e5d1d12c1bb6b4684abef4876e
ashley <ashley.nguyen@avepoint.com>
2022-12-07 12:43:49 +0700

    fix responsive

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
-
-
- const listCard = [
-  { 
+const listCard = [
+  {
     id: 1,
     title: "Podcasts",
     img: "https://i.scdn.co/image/567158eb895ad26718a814345af0fc43ee785ec5",
@@ -50,23 +48,31 @@ import React from "react";
 ];
 
 function Search() {
-  return <div className="mx-4 mt-8 mb-12 ">
-      <div className="text-white text-xl font-bold">
-        Browse all
-      </div>
-      <div className="grid grid-cols-3 gap-6 mt-4 lg:grid-cols-6 md:grid-cols-4">
-       {listCard.map((item, index) => {
-            return (
-              <div key={index} className={`${item.color} h-32 w-auto lg:h-187 lg:w-187 rounded-md overflow-hidden cursor-pointer`}>
-               <div className="text-white text-xl font-bold mt-5 ml-3 mb-5">
-                 {item.title}
-               </div>
-              <img className="rotate-24 h-2/4 mt-12 ml-28" src={item.img} alt="post" />
+  return (
+    <div className="mx-4 mt-8 mb-12 ">
+      <div className="text-white text-xl font-bold">Browse all</div>
+      <div className="grid grid-cols-3 gap-6 mt-4 lg:grid-cols-4 2xl:grid-cols-6 ">
+        {listCard.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className={`${item.color} h-56 w-auto lg:h-204 lg:w-204 rounded-md overflow-hidden cursor-pointer`}
+            >
+              <div className="text-white text-xl font-bold mt-5 ml-3 mb-5">
+                {item.title}
               </div>
-            );
-          })}
-        </div>
-  </div>;
+              <img
+                className="rotate-24 h-2/4 md:mt-16 md:ml-32 lg:mt-20 lg:ml-36 2xl:mt-16 2xl:ml-28"
+                src={item.img}
+                alt="post"
+              />
+              -
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Search;
