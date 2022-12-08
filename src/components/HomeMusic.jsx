77ec86c2848be5321c5746a93ff355d116500f24
ducanh11092001@gmail.com <ducanh11092001@gmail.com>
2022-12-08 12:04:58 +0700

    minor: remove redundant code

--- src/components/HomeMusic.jsx
+++ src/components/HomeMusic.jsx
@@ -4,39 +4,6 @@ import { actionType } from "../context/reducer";
 import { useStateValue } from "../context/StateProvider";
 import MusicCard from "./MusicCard";
 
-// const songs = [
-//   {
-//     name: "I don't wanna live forever",
-//     imageURL:
-//       "https://upload.wikimedia.org/wikipedia/en/8/82/Zayn_%26_Taylor_Swift_-_I_Don%27t_Wanna_Live_Forever_%28Official_Single_Cover%29.png",
-//     artist: "Zayn, Taylor Swift",
-//   },
-//   {
-//     name: "I don't wanna live forever",
-//     imageURL:
-//       "https://upload.wikimedia.org/wikipedia/en/8/82/Zayn_%26_Taylor_Swift_-_I_Don%27t_Wanna_Live_Forever_%28Official_Single_Cover%29.png",
-//     artist: "Zayn, Taylor Swift",
-//   },
-//   {
-//     name: "I don't wanna live forever",
-//     imageURL:
-//       "https://upload.wikimedia.org/wikipedia/en/8/82/Zayn_%26_Taylor_Swift_-_I_Don%27t_Wanna_Live_Forever_%28Official_Single_Cover%29.png",
-//     artist: "Zayn, Taylor Swift",
-//   },
-//   {
-//     name: "I don't wanna live forever",
-//     imageURL:
-//       "https://upload.wikimedia.org/wikipedia/en/8/82/Zayn_%26_Taylor_Swift_-_I_Don%27t_Wanna_Live_Forever_%28Official_Single_Cover%29.png",
-//     artist: "Zayn, Taylor Swift",
-//   },
-//   {
-//     name: "I don't wanna live forever",
-//     imageURL:
-//       "https://upload.wikimedia.org/wikipedia/en/8/82/Zayn_%26_Taylor_Swift_-_I_Don%27t_Wanna_Live_Forever_%28Official_Single_Cover%29.png",
-//     artist: "Zayn, Taylor Swift",
-//   },
-// ];
-
 export const SongContainer = ({ title, songs }) => (
   <div className="mb-8">
     <p className="mb-4 font-bold text-white text-xl">{title}</p>
@@ -55,7 +22,6 @@ function HomeMusic() {
       dispatch({ type: actionType.SET_ALL_SONGS, allSongs: songData.data });
     });
   }, [dispatch]);
-  console.log(allSongs);
   return (
     <div className="p-8 pt-0">
       {allSongs && (
