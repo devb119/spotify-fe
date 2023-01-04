ee92670835df0c8ea947d13144d30b68ac1bdee6
ducanh11092001@gmail.com <ducanh11092001@gmail.com>
2023-01-04 23:05:24 +0700

    revert mt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="ml-64 w-full ">
         <Header />
 
-        <div className="mt-20">
+        <div className="mt-28">
           <Outlet></Outlet>
         </div>
         <div className="h-16"></div>
