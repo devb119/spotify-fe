bf07168bcd03d7062d322e170928b9b0587181b1
ducanh11092001@gmail.com <ducanh11092001@gmail.com>
2023-01-08 11:43:58 +0700

    handle like song in song row

--- src/components/SongRow.jsx
+++ src/components/SongRow.jsx
@@ -6,9 +6,11 @@ import { RiHeartFill, RiHeartLine } from "react-icons/ri";
 import { actionType } from "../context/reducer";
 import { useStateValue } from "../context/StateProvider";
 import Equalizer from "./Equalizer";
-function SongRow({ song, id, toggleLikeSong, type = 1 }) {
+import { addLikedSongs, deleteLikedSongs } from "../api";
+
+function SongRow({ song, id, type = 1 }) {
   const [isHovered, setIsHovered] = React.useState(false);
-  const [{ isSongPlaying, currentSong, likedSongs }, dispatch] =
+  const [{ isSongPlaying, currentSong, likedSongs, user }, dispatch] =
     useStateValue();
 
   const play = () => {
@@ -19,7 +21,25 @@ function SongRow({ song, id, toggleLikeSong, type = 1 }) {
       dispatch({ type: actionType.SET_CURRENT_SONG, currentSong: song });
     }
   };
-  const pause = () => {};
+  const pause = () => {
+    dispatch({ type: actionType.SET_IS_SONG_PLAYING, isSongPlaying: false });
+  };
+
+  async function handleLikeSong() {
+    if (likedSongs.find((el) => el._id === song._id)) {
+      const newLikedSongs = await deleteLikedSongs(song._id, user.token);
+      dispatch({
+        type: actionType.SET_LIKED_SONGS,
+        likedSongs: newLikedSongs.data,
+      });
+    } else {
+      const newLikedSongs = await addLikedSongs(song._id, user.token);
+      dispatch({
+        type: actionType.SET_LIKED_SONGS,
+        likedSongs: newLikedSongs.data,
+      });
+    }
+  }
   return (
     <div
       className="py-2 hover:bg-neutral-800 text-textColor font-medium grid grid-cols-12 text-xs gap-1 items-center rounded"
@@ -104,19 +124,15 @@ function SongRow({ song, id, toggleLikeSong, type = 1 }) {
 
       <div className="col-span-1 text-center"> </div>
       <div className="col-span-1 text-center flex items-center">
-        {likedSongs.includes(song._id) === true ? (
+        {likedSongs.find((el) => el._id === song._id) ? (
           <RiHeartFill
             className="fill-green-700 text-base m-2 mr-4 hover:cursor-pointer"
-            onClick={() => {
-              toggleLikeSong(song.id);
-            }}
+            onClick={handleLikeSong}
           ></RiHeartFill>
         ) : (
           <RiHeartLine
             className="text-base m-2 hover:cursor-pointer mr-4"
-            onClick={() => {
-              toggleLikeSong(song.id);
-            }}
+            onClick={handleLikeSong}
           ></RiHeartLine>
         )}
         <div>
