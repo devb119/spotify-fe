61850e2852af058c102598625b0babe775350566
ashley <ashley.nguyen@avepoint.com>
2023-01-11 11:04:42 +0700

    add state pausing

--- src/context/initialState.js
+++ src/context/initialState.js
@@ -1,10 +1,12 @@
 export const initialState = {
   user: null,
   isSongPlaying: false,
+  isSongPausing: false,
   allSongs: null,
   currentSong: null,
   miniPlayer: false,
   query: "",
+  searchType: "song",
   playlists: null,
   currentColor: { hex: "#50d71e" },
 };
