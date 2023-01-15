f9c4ac9731fc37f3d21108137d5f09b1a12a48b8
ducanh11092001@gmail.com <ducanh11092001@gmail.com>
2023-01-15 12:57:17 +0700

    split confirm box

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -20,6 +20,8 @@ import Icon from "../assets/img/Icon";
 import { valueDropDown2 } from "../utils/styles";
 import DropDown from "./DropDown";
 import { deletePlaylist } from "../api";
+import ConfirmBox from "./ConfirmBox";
+
 function Search({ query, handleInputOnchange, songs, addToPlaylist }) {
   return (
     <>
@@ -243,38 +245,13 @@ function Playlist() {
           ></Search>
         </>
       )}
-     {modal && (
-        <form className="modal">
-          <div className="overlay" onClick={toggleModal}></div>
-          <div className="modal-content bg-white w-[420px]">
-              <div className="pb-6 px-4">
-                <div className="text-black text-xl font-bold">
-                  Delete from Library?
-                </div>
-                <div className="text-black text-sm font-bold pt-5 pb-3">
-                 { `This will delete ${playlist.name} from Your Library.`}
-                </div>
-              </div>
-              <div className="flex justify-end">
-                  <button
-                    className="bg-white text-black hover:font-bold font-semibold py-3 px-7 mr-4 rounded-full"
-                    type="button"
-                    onClick={toggleModal}
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    className="bg-[#1ed760] text-black hover:bg-[#1fdf64] font-semibold hover:font-bold mr-4 py-3 px-7 rounded-full"
-                    type="button"
-                    onClick={handleDeletePlaylist}
-                  >
-                    Delete
-                  </button>
-              </div>
-            </div>
-        </form>
+      {modal && (
+        <ConfirmBox
+          playlistName={playlist.name}
+          handleDeletePlaylist={handleDeletePlaylist}
+          toggleModal={toggleModal}
+        />
       )}
-
     </div>
   );
 }
